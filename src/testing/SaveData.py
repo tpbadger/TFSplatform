929fffb893154871e3d7557a27f444b64cb39cd7
tpbadger <tombadger12@gmail.com>
2019-08-13 11:03:27 +0100

    logging program written

---
+++ src/testing/SaveData.py
@@ -0,0 +1,36 @@
+import serial
+import time
+import os
+# Set serial port options
+ser = serial.Serial("/dev/cu.usbserial-1410")
+ser.baudrate = 9600
+ser.timeout = 1
+
+data_file = open('data.csv', 'w')
+
+log = True
+
+time.sleep(5) # wait for 5 secs to sync with port
+
+while log:
+    try:
+        payload = ser.readline().decode('utf-8')
+        data_file.write(payload)
+    except:
+        data_file.close()
+        log = False
+
+
+data_file.close()
+
+new_fn = time.time()
+current_dir = os.getcwd()
+
+os.rename(current_dir + "/data.csv", current_dir + "/Data/data_" + str(new_fn) + ".csv")
+
+
+
+
+
+
+
